7eae044d85862b992b92407fa692433c1de8dc5e
Yonas <ytilahun15@alustudent.com>
2019-04-06 23:52:47 +0400

    new model: User

---
+++ src/main/java/com/milestoneplanner/model/User.java
@@ -0,0 +1,53 @@
+package com.milestoneplanner.model;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class User {
+    @SuppressWarnings("unused")
+    static final Logger LOG = LoggerFactory.getLogger(Milestone.class);
+
+    private String fName;
+    private String lName;
+    private String email;
+    private String password;
+
+    public User(String fName, String lName, String email, String password) {
+        this.fName = fName;
+        this.lName = lName;
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getfName() {
+        return fName;
+    }
+
+    public void setfName(String fName) {
+        this.fName = fName;
+    }
+
+    public String getlName() {
+        return lName;
+    }
+
+    public void setlName(String lName) {
+        this.lName = lName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
